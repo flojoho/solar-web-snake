cec37993c6ebe98186bec299bca2467a4fa18c1e
flojoho <hofkas.flo@gmx.at>
2022-09-21 23:43:44 +0200

    no favicon i guess :(

--- modules/Bubble.js
+++ modules/Bubble.js
@@ -1,24 +1,17 @@
 import { bubbleRadius, snakeSpeed } from './globals.js';
 import { ctx }  from './globals.js';
 
-const icon = new Image();
-icon.src = '../img/fronius_favicon.svg';
-
 class Bubble {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 
-  draw(isHead) {
-    if(isHead) {
-      ctx.drawImage(icon, this.x - bubbleRadius, this.y - bubbleRadius, 2 * bubbleRadius, 2 * bubbleRadius);
-    } else {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, bubbleRadius, 0, 2 * Math.PI, false);
-      ctx.fillStyle = '#ffb400';
-      ctx.fill();
-    }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, bubbleRadius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = '#ffb400';
+    ctx.fill();
   }
 
   move(direction) {
